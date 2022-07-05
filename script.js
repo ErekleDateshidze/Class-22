23aa01c1e9a28f7c54482ccb133ff8d32f15c134
Erekle99 <erekledateshidze99@gmail.com>
2022-07-06 00:26:59 +0400

    1
    1

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ class DataTable {
         const data = await fetch("./tableData.json")
         const parsed = await data.json()
         this.data = parsed;
+        this.buildTable();
     }
 
 
     buildTable() {
-        const headers = Object.keys.this.data[0]
+        const headers = Object.keys(this.data[0])
+            .map((headers) => {
+                return `<th>${headers}</th>`
+            })
+            .join("")
+
+
+        const rows = this.data.slice(1).map((row) => {
+            const values = Object.values(row)
+                .map((values) => {
+                    return `<tr>${values}</tr>`
+                })
+                .join("")
+            return `<tr>${values}</tr>`
+
+        })
+        this.el.innerHTML = `
+            <thead>${headers}</thead>
+            <tbody>${rows}</tbody>
+        `
     }
 }
+
+new DataTable();
---
+++ script2.js
@@ -0,0 +1,27 @@
+// let num = [-1, -10, -100, -133, 3, 5, 20, 30, 50, 17];
+
+
+// function secondBigest() {
+//     var firstSmallest = Math.min(...num);
+//     var position = num.indexOf(firstSmallest);
+//     num.splice(position, 1);
+//     var answer = Math.min(...num);
+//     console.log(answer)
+// }
+
+// secondBigest();
+
+
+
+// var submit = document.querySelector('.btn');
+// var input = document.querySelector(".input")
+// submit.addEventListener('click', getDate)
+
+// function getDate(e) {
+//     e.preventDefault()
+//     var data = new Date()
+//     var day = data.getDay()
+//     var month = data.getMonth()
+//     var monthday = data.getDate()
+//     console.log(day + ' ' + month + ' ' + monthday)
+// }
